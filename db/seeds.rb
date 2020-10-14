4261971a789072c61fb8861db8cefb8551de990f
Alexandre Castello <aaca91@gmail.com>
2020-10-14 15:31:11 -0300

    Seed using JSON parse implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+#JSON PARSE
+filepath = 'cores.json'
+serialized_colors = File.read(filepath)
+json_hash = JSON.parse(serialized_colors)
+
+#TABLE INPUS
+json_hash['value'].each do |color| 
+  new_color = Color.new
+  new_color.name = color["descricao"]
+  new_color.code = color["codigo"]
+  new_color.integration_id = color["cor"]
+  new_color.save
+end
+
+
